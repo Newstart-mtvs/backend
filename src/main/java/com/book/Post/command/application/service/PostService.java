db2ff428e7800afb32dd667f2551df276c917b82
horry10 <horry10@naver.com>
2023-08-16 20:41:38 +0900

    default folder

--- src/main/java/com/book/Post/command/application/service/PostService.java
+++ src/main/java/com/book/Post/command/application/service/PostService.java
@@ -1,4 +1,19 @@
 package com.book.Post.command.application.service;
 
+import com.book.Post.command.application.dto.PostDTO;
+import com.book.Post.command.domain.repository.PostRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
 public class PostService {
+    private PostRepository postRepository;
+
+    public PostService(PostRepository postRepository) {
+        this.postRepository = postRepository;
+    }
+
+
+
+
 }
